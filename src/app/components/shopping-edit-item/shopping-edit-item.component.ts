b656bafb6b3fb392091b67b8d1be29229fa465c1
Luis Hernandez <lhdez@iuma.ulpgc.es>
2023-04-24 18:24:55 +0100

    cambios menores

--- src/app/components/shopping-edit-item/shopping-edit-item.component.ts
+++ src/app/components/shopping-edit-item/shopping-edit-item.component.ts
@@ -1,22 +1,17 @@
-import {Component, EventEmitter, Input,
-  OnChanges, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit-item',
   templateUrl: './shopping-edit-item.component.html',
   styleUrls: ['./shopping-edit-item.component.css']
 })
-export class ShoppingEditItemComponent implements OnChanges {
+export class ShoppingEditItemComponent  {
 
   @Input()
   itemName = '';
 
   @Output()
   updateItemEvent = new EventEmitter<string>();
-
-  ngOnChanges(changes: SimpleChanges): void {
-
-  }
 
   updateItem() {
 
